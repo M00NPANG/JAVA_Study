7e9c3a9f01f81aa62b7b82bc79f1daec89ef7b50
gwony <moonsky1124@naver.com>
2024-12-19 17:28:10 +0900

    MustHave_Jave Chapter 02 선수 수업

---
+++ src/MustHave/Part01/Exam01.java
@@ -0,0 +1,13 @@
+package MustHave.Part01;
+
+public class Exam01 {
+    public static void main(String[] args) {
+        System.out.println("홍길동");
+
+        System.out.print("전우치");
+
+        System.out.println();
+
+        System.out.print("손오공");
+    }
+}
---
+++ src/MustHave/Part01/Exam02.java
@@ -0,0 +1,17 @@
+package MustHave.Part01;
+
+public class Exam02 {
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 3;
+
+        // 계산 결과를 변수에 대입
+        int result = num1 + num2;
+
+        // 변수에 저장된 값을 출력
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(result);
+    }
+}
